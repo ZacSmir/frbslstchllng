77e55c66fc94d564fb20eac466ae48b9881cf30b
ZacSmir <Zach@smirin.us>
2016-10-05 08:37:45 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var input = $("#new-item-input")
 
 input.keydown(function(e) {	
+  console.log(""
   if (e.keyCode == "13") {
+    console.log("input")
     var valueToSave = input.val();
     addListItem(valueToSave);
   }
